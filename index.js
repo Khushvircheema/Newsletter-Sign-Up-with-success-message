5bf7eb0a12f7d9cbd10ccf2a9ec8bc0e9dd4cc57
Khushvircheema <khushvircheema@icloud.com>
2023-06-27 21:59:55 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+const signUpCard = document.querySelector(".card");
+const successCard = document.querySelector(".cardSuccess");
+const dismiss = document.querySelector(".dismiss");
+const email = document.querySelector(".email");
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  signUpCard.classList.add("hide");
+  successCard.classList.remove("hide");
+  console.log(email.value);
+  document.querySelector("strong").innerText = email.value;
+})
+function homepage() {
+  window.location.href = "/";
+}
+
